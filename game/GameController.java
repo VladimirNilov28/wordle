b4d284d500c128d56bef08f8d5f45e69b57931a9
student <kooduser@pupil.kood.tech>
2024-11-02 23:06:47 +0200

    hello

--- game/GameController.java
+++ game/GameController.java
@@ -28,11 +28,13 @@ public class GameController {
     private void updateRemainingLetters(String guess) {
         for (char c : guess.toUpperCase().toCharArray()) {
             if (remainingLetters.contains(String.valueOf(c))) {
-                remainingLetters = remainingLetters.replace(c + " ", "").replace(String.valueOf(c), "");
+                // Удаление буквы и лишнего пробела
+                remainingLetters = remainingLetters.replace(c + " ", "");
+                remainingLetters = remainingLetters.replace(String.valueOf(c), "");
             }
         }
-        // Форматируем оставшиеся буквы с пробелами
-        remainingLetters = String.join(" ", remainingLetters.trim().split(""));
+        // Убедись, что все оставшиеся буквы идут через пробелы
+        remainingLetters = String.join(" ", remainingLetters.split(" "));
     }
 
 
